032136aa8e84efaa7a3d3130acd6ee531b8698a2
TomHEvans <tom.h.evans.2@outlook.com>
2026-09-09 07:08:13 +0100

    Register 2026-wk37 and set currentWeekId to 2026-wk37

--- src/plans/index.js
+++ src/plans/index.js
@@ -10,6 +10,7 @@ import wk32 from './2026-wk32.js'
 import wk34 from './2026-wk34.js'
 import wk35 from './2026-wk35.js'
 import wk36 from './2026-wk36.js'
+import wk37 from './2026-wk37.js'
 
 export const weeks = {
   [wk28.weekId]: wk28,
@@ -20,6 +21,7 @@ export const weeks = {
   [wk34.weekId]: wk34,
   [wk35.weekId]: wk35,
   [wk36.weekId]: wk36,
+  [wk37.weekId]: wk37,
 }
 
-export const currentWeekId = '2026-wk36'
+export const currentWeekId = '2026-wk37'
